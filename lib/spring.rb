ff56c95be57f29007b64b843cc9b31901d05579c
Krisna Pranav <krisna.pranav@gmail.com>
2022-04-15 19:21:14 +0530

    add files via upload

---
+++ lib/spring.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "spring/version"
+
+module Spring
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/spring/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Spring
+  VERSION = "0.1.0"
+end
