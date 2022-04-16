8a6d7c67fa26cd4e6c6e595aa1f159d42633db4d
Krisna Pranav <krisna.pranav@gmail.com>
2022-04-16 09:17:21 +0530

    lib: spring render[has_cache]

--- lib/spring/render/has_cache.rb
+++ lib/spring/render/has_cache.rb
@@ -1,21 +1,33 @@
 module Spring
     module Base
-        module HasCache
-            def self.extended(base)
-                base.instance_variable_set :@_lock, Mutex.new
-                base.instance_variable_set :@_cache, {}.dup
-            end
 
-            def store(key, value)
-                @_lock.synchronize { @_clock[key] = value }
-            end
+       module HasCache
+          def self.extended(base)
+             base.instance_variable_set :@_lock, Mutex.new
+             base.instance_variable_set :@_cache, {}.dup
+          end
+ 
+          def store(key, value)
+             @_lock.synchronize { @_cache[key] = value }
+          end
+          alias []= store
+          def get(key)
+             @_lock.synchronize { @_cache[key] }
+          end
+          alias [] get
+       end
 
-            alias []=store
+       module HasStore
 
-            def get(key)
-                @_lock.synchronize { @_cache[key] }
-            end
-            alias [] get
-        end
+          def store(key, value)
+             (Thread.current[(@_chache_name ||= object_id.to_s(16))] ||= {}.dup)[key] = value
+          end
+          alias []= store
+
+          def get(key)
+             (Thread.current[(@_chache_name ||= object_id.to_s(16))] ||= {}.dup)[key]
+          end
+          alias [] get
+       end
     end
-end
+ end
