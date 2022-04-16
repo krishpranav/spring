035f072273b9fd86331b30b1951a66f9ceab3c1d
Krisna Pranav <krisna.pranav@gmail.com>
2022-04-16 08:54:08 +0530

    lib: spring[activation]

--- lib/spring/activation.rb
+++ lib/spring/activation.rb
@@ -1,23 +1,37 @@
 require 'uri' unless defined?(::URI)
 
 module Spring
-  protected
+   protected
 
   @spring_finalize = nil
+
   def spring_finalize
-    if @spring_finalize = true
-      @spring_finalize = 1
-    end
+     if @spring_finalize.nil?
+        @spring_finalize = true
+        @spring_finalize = 1
+     end
   end
 
   @spring_initialize = nil
+  
   def self.spring_initialize
-    if @spring_initialize.nil?
-      @spring_initialize = true
-      self.has_proc_4symstr
-      @spring_autostart = true if @spring_autostart.nil?
-      Iodine.patch_rack
-
-    end
+     if @spring_initialize.nil?
+        @spring_initialize = true
+        self.hash_proc_4symstr 
+        @spring_autostart = true if @spring_autostart.nil?
+        Iodine.patch_rack
+        if((ENV['PL_REDIS_URL'.freeze] ||= ENV['REDIS_URL'.freeze]))
+          ping = ENV['PL_REDIS_TIMEOUT'.freeze] || ENV['REDIS_TIMEOUT'.freeze]
+          ping = ping.to_i if ping
+          Iodine::PubSub.default = Iodine::PubSub::RedisEngine.new(ENV['PL_REDIS_URL'.freeze], ping: ping)
+          Iodine::PubSub.default = Iodine::PubSub::CLUSTER unless Iodine::PubSub.default
+        end
+        at_exit do
+           next if @spring_autostart == false
+           ::Iodine.listen2http app: ::Spring.app
+           ::Iodine.start
+        end
+     end
+     true
   end
 end
